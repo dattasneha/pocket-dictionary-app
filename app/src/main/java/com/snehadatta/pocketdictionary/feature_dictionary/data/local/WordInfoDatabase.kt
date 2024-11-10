026c4c4e0af34c7d6eba0cbc13f972ad0a56b81e
Sneha Datta <snehadatta431@gmail.com>
2024-11-10 11:51:07 +0530

    Setup room database and add type converters

--- app/src/main/java/com/snehadatta/pocketdictionary/feature_dictionary/data/local/WordInfoDatabase.kt
+++ app/src/main/java/com/snehadatta/pocketdictionary/feature_dictionary/data/local/WordInfoDatabase.kt
@@ -2,12 +2,14 @@ package com.snehadatta.pocketdictionary.feature_dictionary.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.snehadatta.pocketdictionary.feature_dictionary.data.local.entities.WordInfoEntity
 
 @Database(
     entities = [WordInfoEntity::class],
     version = 1
 )
+@TypeConverters(Converters::class)
 abstract class WordInfoDatabase:RoomDatabase() {
 
     abstract val dao: WordInfoDao
